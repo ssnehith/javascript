e9b41567203a94099e5382bc3fc6195d8bdfa882
Snehith <ssssnehith07@gmail.com>
2026-06-29 22:45:01 +0530

    getting elements by id

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ alert("welcome")
 console.log("Welcome")
 
 document.body.style.backgroundColor="grey";
+
+let ChangeText=document.getElementById("title");
+
+ChangeText.textContent="JavaScript is the Dynamic Language";
+
+ChangeText.style.color="blue"
