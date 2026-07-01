7c57448b3f25418149eda24fe8938fbf287a007b
Snehith <ssssnehith07@gmail.com>
2026-07-01 22:10:42 +0530

    operators

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-console.log("Hello")
+// console.log("Hello")
 
-alert("welcome")
+// alert("welcome")
 
-console.log("Welcome")
+// console.log("Welcome")
 
-document.body.style.backgroundColor="grey";
+// document.body.style.backgroundColor="grey";
 
-let ChangeText=document.getElementById("title");
+// let ChangeText=document.getElementById("title");
 
-let ChangeText1=document.querySelector("title");
-
-
-ChangeText.textContent="JavaScript is the Dynamic Language";
+// let ChangeText1=document.querySelector("title");
 
 
-// let btn=document.getElementById("btn")
+// ChangeText.textContent="JavaScript is the Dynamic Language";
+
+
+// // let btn=document.getElementById("btn")
+
+// function changeHeading(){
+// ChangeText.style.color="blue"
+// ChangeText.textContent="20"
+// }
+
+// let bottle="water"
+
+// console.log(bottle)
+
+// bottle="Milk"
+
+// console.log(bottle)
+
+
+// const fruit="apple"
+
+// console.log(fruit)
+
+// fruit="banana"
+
+// console.log(fruit)
+
+// var a=10
+
+// a=20
+
+// console.log(10)
 
 function changeHeading(){
-ChangeText.style.color="blue"
+
+let output=document.getElementById("value")
+
+output.textContent=20
+
+const output1=document.getElementById("value1")
+
+output1.textContent=40
+
+var output2=document.getElementById("value2")
+
+output2.textContent=60
+
 }
-
-let bottle="water"
-
-console.log(bottle)
-
-bottle="Milk"
-
-console.log(bottle)
-
-
-const fruit="apple"
-
-console.log(fruit)
-
-fruit="banana"
-
-console.log(fruit)
-
-var a=10
-
-a=20
-
-console.log(10)
