6f27ec736e72d27b8c506010df86034b42788a2e
Snehith <ssssnehith07@gmail.com>
2026-07-01 22:21:47 +0530

    conditional statements

--- operators.js
+++ operators.js
@@ -1,15 +1,21 @@
 let a= 10
 let b= 50
 
-let c=a+b
+// let c=a+b
 
-let d=a-b
+// let d=a-b
 
-let e=a*b
+// let e=a*b
 
-let f=a/b
+// let f=a/b
 
-console.log(c)
-console.log(d)
-console.log(e)
-console.log(f)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+
+console.log(a>b)
+
+console.log(a<b)
+
+console.log(a==b)
