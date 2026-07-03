c9fa0e4ee4b3ae21a462dae551bbc16d9e00b24e
Snehith <ssssnehith07@gmail.com>
2026-07-03 22:11:13 +0530

    interpolation

--- loops.js
+++ loops.js
@@ -6,6 +6,14 @@ for(let i=2;i<20;i+=2){
     console.log("even nums:",i);
 }
 
+let i=1
 while(i<=5){
     console.log(i);
+    i++;
+}
+
+let num=2;
+
+for(let i=1;i<=10;i++){
+    console.log(`${num}x${i}=${num*i}`)
 }
