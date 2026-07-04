34406fc7122ad33d7099026efa296fedce8ee98b
Snehith <ssssnehith07@gmail.com>
2026-07-04 23:23:07 +0530

    Add basic for loop number sequence

--- loops.js
+++ loops.js
@@ -1,19 +1,12 @@
-for(let i=1;i<4;i++){
-    console.log(i)
+const output = document.getElementById("output");
+
+function printMessage(message) {
+    output.textContent += message + "\n";
+    console.log(message);
 }
 
-for(let i=2;i<20;i+=2){
-    console.log("even nums:",i);
-}
+printMessage("Basic number sequence:");
 
-let i=1
-while(i<=5){
-    console.log(i);
-    i++;
-}
-
-let num=2;
-
-for(let i=1;i<=10;i++){
-    console.log(`${num}x${i}=${num*i}`)
+for (let i = 1; i <= 5; i++) {
+    printMessage(`Number: ${i}`);
 }
