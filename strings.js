61a32ea56e21c12e1c8e17e7bb4a7648e047de01
Snehith <ssssnehith07@gmail.com>
2026-07-10 22:59:43 +0530

    added string character access

--- strings.js
+++ strings.js
@@ -5,3 +5,6 @@ console.log("String length:", language.length);
 
 console.log("Uppercase:", language.toUpperCase());
 console.log("Lowercase:", language.toLowerCase());
+
+console.log("First character:", language.charAt(0));
+console.log("Last character:", language.charAt(language.length - 1));
