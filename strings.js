37531384b420c1769e43e1937dde4e7e3662eb8e
Snehith <ssssnehith07@gmail.com>
2026-07-10 22:58:46 +0530

    string case conversion method

--- strings.js
+++ strings.js
@@ -2,3 +2,6 @@ const language = "JavaScript";
 
 console.log(language);
 console.log("String length:", language.length);
+
+console.log("Uppercase:", language.toUpperCase());
+console.log("Lowercase:", language.toLowerCase());
