f8ab70f6d8244b378967b113f633b141306b133a
Snehith <ssssnehith07@gmail.com>
2026-07-10 23:02:16 +0530

    string search and modification methods

--- strings.js
+++ strings.js
@@ -8,3 +8,7 @@ console.log("Lowercase:", language.toLowerCase());
 
 console.log("First character:", language.charAt(0));
 console.log("Last character:", language.charAt(language.length - 1));
+
+console.log("Contains Script:", language.includes("Script"));
+console.log("Sliced value:", language.slice(0, 4));
+console.log("Replaced value:", language.replace("Java", "Type"));
