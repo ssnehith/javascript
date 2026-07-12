6cc139c65d4b20381fdae3f854bb2c3916bf2cf3
Snehith <ssssnehith07@gmail.com>
2026-07-12 22:33:13 +0530

    added javaScript with page elements

---
+++ random.js
@@ -0,0 +1,4 @@
+const minNumberInput = document.getElementById("minNumber");
+const maxNumberInput = document.getElementById("maxNumber");
+const generateButton = document.getElementById("generateButton");
+const result = document.getElementById("result");
