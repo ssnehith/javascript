68ea789d87f9de13b8057fba221e9b4bd3d79da7
Snehith <ssssnehith07@gmail.com>
2026-07-12 22:34:30 +0530

    Read number inputs on button click

--- random.js
+++ random.js
@@ -2,3 +2,11 @@ const minNumberInput = document.getElementById("minNumber");
 const maxNumberInput = document.getElementById("maxNumber");
 const generateButton = document.getElementById("generateButton");
 const result = document.getElementById("result");
+
+generateButton.addEventListener("click", function () {
+    const minNumber = Number(minNumberInput.value);
+    const maxNumber = Number(maxNumberInput.value);
+
+    console.log("Minimum:", minNumber);
+    console.log("Maximum:", maxNumber);
+});
