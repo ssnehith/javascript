730d696d4c9a4d9d63f40d0ffbaca7fb05f77354
Snehith <ssssnehith07@gmail.com>
2026-07-13 23:20:37 +0530

    filter and sort

--- arraymethods.js
+++ arraymethods.js
@@ -2,3 +2,20 @@ const prices=[100,200,300]
 
 const finalPrices=prices.map(price=>price*1.8);
 console.log(finalPrices)
+
+const products=[
+    {name:"Laptop",price:50000},
+    {name:"Phone",price:20000},
+    {name:"Watch",price:5000}
+];
+
+const productNames=products.map(product=>product.name);
+console.log(productNames)
+
+const marks=[30,45,90,80,20]
+
+const passed=marks.filter(mark=>mark>=45);
+console.log(passed)
+const passedSort=passed.sort((a,b)=>a-b)
+console.log(passedSort)
+
