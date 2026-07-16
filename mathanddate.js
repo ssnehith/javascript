26d6b2a27283fb46ff77999ac7602e3d9a343167
Snehith <ssssnehith07@gmail.com>
2026-07-16 20:32:16 +0530

    math floor added and dice created

--- mathanddate.js
+++ mathanddate.js
@@ -1,3 +1,8 @@
 console.log(Math.PI);
 console.log(Math.random());
 
+//Floor
+console.log(Math.floor(10.2));
+console.log(Math.floor(10.9));
+console.log(Math.floor(-10.9));
+console.log(Math.floor(Math.random()*6)+1);
