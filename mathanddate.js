4ef3da720fa0b721fedc6303e0548328c03b0646
Snehith <ssssnehith07@gmail.com>
2026-07-16 20:36:13 +0530

    otp genartor and lottery picker created

--- mathanddate.js
+++ mathanddate.js
@@ -6,3 +6,22 @@ console.log(Math.floor(10.2));
 console.log(Math.floor(10.9));
 console.log(Math.floor(-10.9));
 console.log(Math.floor(Math.random()*6)+1);
+
+//OTP Generator
+console.log(Math.floor(100000 + Math.random() * 900000));
+
+//Lottery 
+const participants = [
+    "Rahul",
+    "Snehith",
+    "Priya",
+    "Vamika",
+    "Arjun"
+];
+
+const randomIndex = Math.floor(Math.random() * participants.length);
+
+const winner = participants[randomIndex];
+
+console.log("Lottery winner:", winner);
+
